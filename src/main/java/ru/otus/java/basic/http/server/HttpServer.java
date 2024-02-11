8cdb2c3bc13dd7c2cbd0507c2a1784e12f300396
Alsu <alsu1408@yandex.ru>
2024-02-11 23:13:48 +0300

    Homework 23. Добавлено логирование и обработка запросов в параллельных потоках.

--- src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -1,34 +1,62 @@
 package ru.otus.java.basic.http.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
+    private ExecutorService executorService;
+    private static final Logger LOGGER = LogManager.getLogger(HttpServer.class);
 
     public HttpServer(int port) {
         this.port = port;
         this.dispatcher = new Dispatcher();
+        this.executorService = Executors.newFixedThreadPool(10);
     }
 
     public void start() {
+        LOGGER.info("Запуск HTTP сервера на порту {}", port);
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Сервер запущен на порту: " + port);
+            LOGGER.info("Сервер запущен...");
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    byte[] buffer = new byte[8192];
-                    int n = socket.getInputStream().read(buffer);
-                    String rawRequest = new String(buffer, 0, n);
-                    HttpRequest httpRequest = new HttpRequest(rawRequest);
-                    dispatcher.execute(httpRequest, socket.getOutputStream());
+                try{
+                    Socket socket = serverSocket.accept();
+                    executorService.execute(() -> handleClient(socket));
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    LOGGER.error("Ошибка подключения клиента", e);
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error("Ошибка запуска сервера", e);
+        } finally {
+            executorService.shutdown();
+            LOGGER.info("Работа сервера завершена");
+        }
+    }
+
+    private void handleClient(Socket socket) {
+        try {
+            LOGGER.info("Обработка клиентского соединения...");
+            byte[] buffer = new byte[8192];
+            int n = socket.getInputStream().read(buffer);
+            String rawRequest = new String(buffer, 0, n);
+            HttpRequest httpRequest = new HttpRequest(rawRequest);
+            dispatcher.execute(httpRequest, socket.getOutputStream());
+        } catch (IOException e) {
+            LOGGER.error("Ошибка обработки клиентского соединения", e);
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                LOGGER.error("Ошибка закрытия клиентского сокета", e);
+            }
         }
     }
 }
--- src/main/java/ru/otus/java/basic/http/server/MainApplication.java
+++ src/main/java/ru/otus/java/basic/http/server/MainApplication.java
@@ -1,13 +1,17 @@
 package ru.otus.java.basic.http.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MainApplication {
     // Домашнее задание:
     // - Добавить логирование
     // - Добавить обработку запросов в параллельных потоках
-
+    private static final Logger LOGGER = LogManager.getLogger(MainApplication.class);
     public static void main(String[] args) {
         HttpServer server = new HttpServer(Integer.parseInt((String)System.getProperties().getOrDefault("port", "8189")));
         server.start();
+        LOGGER.info("HTTP сервер запущен...");
     }
 }
 
